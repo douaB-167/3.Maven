4960219291f74f324f21188746cf19a905f8f061
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-07-25 14:14:36 +0200

    Add files via upload

--- S15_SpringToMany/src/main/java/com/MyAzienda/SpringToMany/controllers/PhotoController.java
+++ S15_SpringToMany/src/main/java/com/MyAzienda/SpringToMany/controllers/PhotoController.java
@@ -1,6 +1,7 @@
 package com.MyAzienda.SpringToMany.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class PhotoController {
 	@Autowired
 	private String currentDateTimeFormatted;
 	
+	@GetMapping
 	public Iterable<Photo> getAll(){
 		System.out.println("La data-ora corrente nel PhotoController è: " + currentDateTimeFormatted);
 		return photoService.getAll();
--- S15_SpringToMany/src/main/java/com/MyAzienda/SpringToMany/controllers/UserController.java
+++ S15_SpringToMany/src/main/java/com/MyAzienda/SpringToMany/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.MyAzienda.SpringToMany.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class UserController {
 	@Autowired
 	private String currentDateTimeFormatted;
 	
+	@GetMapping
 	public Iterable<User> getAll(){
 		System.out.println("La data-ora corrente nel UserController è: " + currentDateTimeFormatted);
 		return service.getAll();
