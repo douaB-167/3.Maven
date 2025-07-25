af2ae4babe09c879b8809c7436d3029be05f21f9
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-07-25 14:06:42 +0200

    Add files via upload

---
+++ db_photos.sql
@@ -0,0 +1,42 @@
+create database my_photos;
+
+-- selezionare il database my_photos
+use my_photos;
+
+create table photo (
+id int primary key auto_increment,
+-- una chiave primaria è un valore che identifica una riga della tabella
+url text
+);
+
+insert into photo (url)
+values
+("./img/01.png"),
+("./img/02.png"),
+("./img/03.png"),
+("./img/04.png"),
+("./img/05.png"),
+("./img/06.png"),
+("./img/07.png"),
+("./img/08.png"),
+("./img/09.png"),
+("./img/10.png"),
+("./img/11.png"),
+("./img/12.png"),
+("./img/13.png"),
+("./img/14.png"),
+("./img/15.png"),
+("./img/16.png"),
+("./img/17.png"),
+("./img/18.png"),
+("./img/19.png"),
+("./img/20.png"),
+("./img/21.png"),
+("./img/22.png"),
+("./img/23.png"),
+("./img/24.png"),
+("./img/25.png"),
+("./img/26.png"),
+("./img/27.png");
+
+select * from photo;
---
+++ db_s15_springtomany.sql
@@ -0,0 +1,3 @@
+create database S15_SpringToMany;
+use S15_SpringToMany;
+select * from photos;
